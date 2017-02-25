84b517a8da03ad78ef758b59fc3299ef99c13eb4
Oskar Karlsson <kontakta@oskarkarlsson.nu>
2017-02-25 19:03:39 +0100

    🐛 ✨  Compare frame for frame

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { TestScheduler } from 'rxjs/testing/TestScheduler';
-const isEqual = require('lodash.isequal');
+const chalk = require('chalk');
 
 export const createRxTestScheduler = () => new TestScheduler(assertDeepEqualFrame);
 
@@ -11,8 +11,29 @@ function stringifyFrame(x) {
   return `${x.frame}\t${x.notification.kind}\t${value}\t${x.notification.hasValue}`;
 }
 
+function isFrameEqual(aFrame, bFrame) {
+    const aTime = aFrame.frame;
+    const eTime = bFrame.frame;
+    const aKind = aFrame.notification.kind;
+    const eKind = bFrame.notification.kind;
+    const aValue = aFrame.notification.value;
+    const eValue = bFrame.notification.value;
+    const aHasValue = aFrame.notification.hasValue;
+    const eHasValue = bFrame.notification.hasValue;
+    const aError = aFrame.notification.error;
+    const eError = bFrame.notification.error;
+    return (aTime === eTime && aKind === eKind && aValue === eValue && aHasValue === eHasValue && typeof aError === typeof eError);
+}
+
+function isFramesEqual(actual, expected) {
+  if (!(Array.isArray(actual) && Array.isArray(expected) && actual.length === expected.length)) {
+    return false;
+  }
+  return actual.every((aFrame, i) => isFrameEqual(aFrame, expected[i]));
+}
+
 function assertDeepEqualFrame(actual, expected) {
-  const equal = isEqual(actual, expected);
+  const equal = isFramesEqual(actual, expected);
   let message = '';
   if (!equal && Array.isArray(actual) && Array.isArray(expected)) {
     message = '\nExpected \n';
@@ -22,8 +43,8 @@ function assertDeepEqualFrame(actual, expected) {
     });
     message += '\nTo equal \n';
     message += 'frame\tkind\tvalue\thasValue\n';
-    expected.forEach(x => {
-      message += stringifyFrame(x) + '\n';
+    expected.forEach((x, i) => {
+      message += (isFrameEqual(actual[i], x) ? stringifyFrame(x) : chalk.red(stringifyFrame(x))) + '\n';
     });
   }
 
